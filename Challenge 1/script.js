d4922d310950091075cf4f6fafd8e389e07ab7f8
larakoseff <larakoseff@gmail.com>
2023-08-24 13:27:10 +0200

    Solve challenge 1

---
+++ Challenge 1/script.js
@@ -0,0 +1,12 @@
+firstName = 'John';
+age = 35;
+hobby = 'Coding';
+
+
+const logParameterTwice = (parameter) => {
+    console.log(parameter);
+    console.log(parameter);
+}
+
+logParameterTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+
