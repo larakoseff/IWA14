6cab3698ad484b0bf484aff6a8590f11badd66d8
larakoseff <larakoseff@gmail.com>
2023-08-24 13:30:49 +0200

    Small edits

--- Challenge 1/script.js
+++ Challenge 1/script.js
@@ -1,11 +1,11 @@
-firstName = 'John';
-age = 35;
-hobby = 'Coding';
+firstName = 'John'
+age = 35
+hobby = 'Coding'
 
 
 const logParameterTwice = (parameter) => {
-    console.log(parameter);
-    console.log(parameter);
+    console.log(parameter)
+    console.log(parameter)
 }
 
 logParameterTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
