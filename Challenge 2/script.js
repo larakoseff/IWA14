6cc266764e107750dc99a3f7e2e120ce0d3df055
larakoseff <larakoseff@gmail.com>
2023-08-25 10:57:38 +0200

    Challenge 2 solved

---
+++ Challenge 2/script.js
@@ -0,0 +1,37 @@
+// script.js
+
+const add = (a, b) => { return a + b; };
+const multiply = (a, b, c) => { return (a + b) *c };
+
+function internal() {
+    const added = this.add(this.internal.a, this.internal.b);
+    const multiplied = this.multiply(this.internal.a, this.internal.b, this.internal.c);
+    console.log(multiplied);
+}
+
+// Not allowed to change below this
+
+const example1 = {
+	internal: {
+		a: 2,
+		b: 4,
+		c: 8,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+const example2 = {
+	internal: {
+		a: 2,
+		b: 2,
+		c: 3,
+	},
+	add,
+	multiply,
+  calculate: internal
+}
+
+example1.calculate()
+example2.calculate()
